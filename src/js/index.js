a1bfd46a0d53f20455299bd98ea7d1297c68c18c
Mariam <manoukyan.mariam@gmail.com>
2019-03-27 13:40:51 +0400

    Homework number 2 set up

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store/index";
 import App from "./components/App.jsx";
-// if you're in create-react-app import the files as:
-// import store from "./js/store/index";
-// import App from "./js/components/App.jsx";
-render(
+
+ReactDOM.render(
 <Provider store={store}>
     <App />
     </Provider>,
